b3ef32260a353b384547912defc0a20af85b0d1b
Jameson Cotter <jameson.cotter@gmail.com>
2020-08-29 17:11:35 -0400

    adding nginx conf and Dockerfile.

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM nginx:1.16.0-alpine
+COPY ./dist /usr/share/nginx/html
+RUN rm /etc/nginx/conf.d/default.conf
+COPY nginx.conf /etc/nginx/conf.d
+EXPOSE 8080
+CMD ["nginx", "-g", "daemon off;"]
--- server.ts
+++ server.ts
@@ -1,17 +1,17 @@
 import { IncomingMessage, ServerResponse } from "http";
-import fs from 'fs';
+import fs from "fs";
 
 const http = require("http");
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-
-    http
-        .createServer(function(req: IncomingMessage, res: ServerResponse) {
-            res.write(html); // write a response to the client
-            res.end(); // end the response
-        })
-        .listen(8080);
+fs.readFile("./dist/public/index.html", function(err, html) {
+  if (err) {
+    throw err;
+  }
+    console.log('GOING TO LISTEN NOW')
+  http
+    .createServer(function(req: IncomingMessage, res: ServerResponse) {
+      res.write(html); // write a response to the client
+      res.end(); // end the response
+    })
+    .listen(8080, () => console.log("LISTENING ON PORT 8080"));
 });
